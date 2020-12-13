40bb4e46ee490dc5c7460f9cd882d53c2e3c6e78
Antonino Natale <antonio.natale97@hotmail.com>
2020-12-13 21:01:55 +0100

    Update sources

--- src/main/java/it/bioagri/api/auth/Auth.java
+++ src/main/java/it/bioagri/api/auth/Auth.java
@@ -29,7 +29,10 @@ import it.bioagri.models.User;
 import it.bioagri.persistence.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
