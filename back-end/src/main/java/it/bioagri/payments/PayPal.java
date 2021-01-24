6d2ef6b6de50a227503dad56a99c982098406cd0
Antonino Natale <antonio.natale97@hotmail.com>
2021-01-24 20:56:56 +0100

    Update sources

--- back-end/src/main/java/it/bioagri/payments/PayPal.java
+++ back-end/src/main/java/it/bioagri/payments/PayPal.java
@@ -25,24 +25,77 @@
 
 package it.bioagri.payments;
 
+import ch.qos.logback.classic.Logger;
+import com.paypal.core.PayPalEnvironment;
+import com.paypal.core.PayPalHttpClient;
+import com.paypal.http.HttpResponse;
+import com.paypal.orders.Order;
+import com.paypal.orders.OrderCaptureRequest;
+import com.paypal.orders.OrderRequest;
+import com.paypal.orders.OrdersCaptureRequest;
+import it.bioagri.api.auth.Auth;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 @Scope("singleton")
 public class PayPal {
 
-    private final String clientId;
+    private final static Logger logger = (Logger) LoggerFactory.getLogger(PayPal.class);
+    private final PayPalHttpClient client;
 
     @Autowired
-    private PayPal(@Value("${payments.services.paypal.id}") String clientId) {
-        this.clientId = clientId;
+    private PayPal(
+            @Value("${payments.services.paypal.id}")     String clientId,
+            @Value("${payments.services.paypal.secret}") String clientSecret
+    ) {
+        this.client = new PayPalHttpClient(new PayPalEnvironment.Sandbox(clientId, clientSecret));
     }
 
-    public String getClientId() {
-        return clientId;
+
+    public PayPalHttpClient getClient() {
+        return client;
+    }
+
+    boolean authorize(String orderId) throws IOException {
+
+        HttpResponse<Order> response = getClient().execute(new OrdersCaptureRequest(orderId) {{
+            requestBody(new OrderRequest());
+        }});
+
+        if(logger.isDebugEnabled()) {
+
+            logger.debug("===== PAYPAL AUTHORIZATION =====");
+            logger.debug("Status Code: %d".formatted(response.statusCode()));
+            logger.debug("Status     : %s".formatted(response.result().status()));
+            logger.debug("OrderId    : %s".formatted(response.result().id()));
+            logger.debug("Links      : %s".formatted(response.result().links().stream().map(i -> "%s: %s".formatted(i.rel(), i.href()))));
+
+            for(var i : response.result().purchaseUnits())
+                for(var j : i.payments().captures())
+                    logger.debug("Captures   : %s".formatted(j.id()));
+
+
+            logger.debug("BuyerMail  : %s".formatted(response.result().payer().email()));
+            logger.debug("BuyerName  : %s".formatted(response.result().payer().name().fullName()));
+
+            logger.debug("BuyerPhone : %s %s".formatted(
+                    response.result().payer().phoneWithType().phoneNumber().countryCallingCode(),
+                    response.result().payer().phoneWithType().phoneNumber().nationalNumber()));
+
+            logger.debug("=====  END AUTHORIZATION   =====");
+
+
+        }
+
+
+        return response.statusCode() == 200;
+
     }
 
 
